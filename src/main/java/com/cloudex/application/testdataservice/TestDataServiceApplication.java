349472a6fa71b50cc44789707042acac50cdc6f1
Partha Sarathi Chaudhuri <parthasarathic@gmail.com>
2019-03-12 17:27:25 +0530

    first commit

---
+++ src/main/java/com/cloudex/application/testdataservice/TestDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloudex.application.testdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudex/application/testdataservice/TestDataServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloudex.application.testdataservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestDataServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
